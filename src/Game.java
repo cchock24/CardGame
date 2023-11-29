e07c4e4d077a8f9e68544c3aa2e9c520d4370ae1
Caden <caden.chock@menloschool.org>
2023-11-29 11:57:19 -0800

    Edited constructor for Card class.

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class Game {
+    int turn = 0;
+    Player[] players;
+
+    public Game()
+    {
+
+    }
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        Game b = new Game();
+        b.makePlayers();
+    }
+
+    public void makePlayers()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("How many Players:");
+        int playerNum = s.nextInt();
+        s.nextLine();
+        players = new Player[playerNum];
+        for(int i = 0; i < playerNum; i++)
+        {
+            System.out.println("What's You Name?");
+            String name = s.nextLine();
+            players[i] = new Player(name);
+        }
+    }
+
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,49 @@
+import java.util.ArrayList;
+
 public class Player {
+
+    private String name;
+    private ArrayList<Card> hand = new ArrayList<Card>();
+    private int point;
+
+    public Player(String n){
+        this.name = n;
+        point = 0;
+    }
+
+    public Player(String n, ArrayList<Card> h){
+        name = n;
+        point = 0;
+        hand = h;
+    }
+
+    public int getPoint()
+    {
+        return point;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public ArrayList<Card> getHand()
+    {
+        return hand;
+    }
+
+    public void addPoints(int points)
+    {
+        point += points;
+    }
+
+    public void addCard(Card card)
+    {
+        hand.add(card);
+    }
+
+    public String toString()
+    {
+        return name + " has " + point + " points" + name + "'s cards: " + hand;
+    }
 }
