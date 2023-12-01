9b022629d4d5b361a56152484e2bd291b6da6800
Caden <caden.chock@menloschool.org>
2023-12-01 09:55:06 -0800

    Edited constructor for Card class.

--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,34 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
     int turn = 0;
     Player[] players;
 
+    private Deck deck;
+    private int playerNum;
+
+    private ArrayList<Card> pile;
+
     public Game()
     {
-
+        Deck deck = new Deck();
+        pile = new ArrayList<Card>();
     }
     public static void main(String[] args) {
-        Deck deck = new Deck();
         Game b = new Game();
-        b.makePlayers();
+        b.playGame(b);
     }
 
+    public void playGame(Game b){
+        b.makePlayers();
+    }
+// Gets Player Name's and Number
     public void makePlayers()
     {
         Scanner s = new Scanner(System.in);
         System.out.println("How many Players:");
-        int playerNum = s.nextInt();
+        playerNum = s.nextInt();
         s.nextLine();
         players = new Player[playerNum];
         for(int i = 0; i < playerNum; i++)
@@ -27,6 +37,19 @@ public class Game {
             String name = s.nextLine();
             players[i] = new Player(name);
         }
+    }
+// Shuffles Decks and Deals
+    public void makeHands()
+    {
+        deck.shuffle();
+        int cardAmount = 52 / playerNum;
+        int cardLeft = 52 % playerNum;
+      // Add Leftover Cards to Pile
+        for(int i = 0; i < cardLeft; i++)
+        {
+            pile.add(deck.deal());
+        }
+
     }
 
 
